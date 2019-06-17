499cfe642085b14ee2f9979451e17ccf0a978635
DzesenKopeykin <kopeykindzzesen@gmail.com>
2019-06-17 16:03:58 +0300

    add the logic of getting all events for day or month

--- RT_Calendar/views.py
+++ RT_Calendar/views.py
@@ -3,7 +3,7 @@ from rest_framework import mixins
 from rest_framework import viewsets
 from .serializers import UserSerializer, EventSerializer
 from .models import Event, User
-import datetime
+from datetime import datetime, time, timedelta
 
 
 class UserViewSet(mixins.CreateModelMixin,
@@ -22,8 +22,15 @@ class EventViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user,
                         end_date=self.request.data['end_date']
                             if 'end_date' in self.request.data else self.request.data['start_date'],
-                        end_time=self.request.data['end_time'] if 'end_time' in self.request.data else datetime.time(23, 59)
+                        end_time=self.request.data['end_time'] if 'end_time' in self.request.data else time(23, 59)
                         )
 
     def get_queryset(self):
-        return Event.objects.filter(user=self.request.user)
+        queryset = Event.objects.filter(user=self.request.user)
+        if 'day' in self.request.data:
+            queryset = queryset.filter(start_date=datetime.strptime(self.request.data['day'], '%Y-%m-%d'))
+        elif 'month' in self.request.data:
+            date1 = datetime.strptime(self.request.data['month'], '%Y-%m')
+            date2 = datetime(date1.year, date1.month + 1, date1.day)
+            queryset = queryset.filter(start_date__range=[date1, date2])
+        return queryset
